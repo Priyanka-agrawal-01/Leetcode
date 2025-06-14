be58c1a4624df01a77d3976978bb12c711c38140
Priyanka <priyankaagrawal9139@gmail.com>
2025-06-14 23:48:55 +0530

    Time: 1 ms (89.2%), Space: 40.5 MB (82.39%) - LeetHub

--- 2566-maximum-difference-by-remapping-a-digit/2566-maximum-difference-by-remapping-a-digit.java
+++ 2566-maximum-difference-by-remapping-a-digit/2566-maximum-difference-by-remapping-a-digit.java
@@ -1,46 +1,16 @@
 class Solution {
+
     public int minMaxDifference(int num) {
-        ArrayList<Integer> al = new ArrayList<>();
-        int temp = num;
-        while (temp != 0) {
-            al.add(temp % 10);
-            temp /= 10;
+        String s = Integer.toString(num);
+        String t = s;
+        int pos = 0;
+        while (pos < s.length() && s.charAt(pos) == '9') {
+            pos++;
         }
-        int replaceMax = -1;
-        for (int i = al.size() - 1; i >= 0; i--) {
-            if (al.get(i) != 9) {
-                replaceMax = al.get(i);
-                break;
-            }
+        if (pos < s.length()) {
+            s = s.replace(s.charAt(pos), '9');
         }
-        int num1 = 0;
-        for (int i = al.size() - 1; i >= 0; i--) {
-            int digit = al.get(i);
-            if (digit == replaceMax) {
-                num1 = num1 * 10 + 9;
-            } else {
-                num1 = num1 * 10 + digit;
-            }
-        }
-
-        int replaceMin = -1;
-        for (int i = al.size() - 1; i >= 0; i--) {
-            if (al.get(i) != 0) {
-                replaceMin = al.get(i);
-                break;
-            }
-        }
-
-        int num2 = 0;
-        for (int i = al.size() - 1; i >= 0; i--) {
-            int digit = al.get(i);
-            if (digit == replaceMin) {
-                num2 = num2 * 10 + 0;
-            } else {
-                num2 = num2 * 10 + digit;
-            }
-        }
-
-        return num1 - num2;
+        t = t.replace(t.charAt(0), '0');
+        return Integer.parseInt(s) - Integer.parseInt(t);
     }
 }
