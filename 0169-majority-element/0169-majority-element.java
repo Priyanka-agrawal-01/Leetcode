11cdc7f1c5c1432e6ff72e5031920a521e618c7e
Priyanka <priyankaagrawal9139@gmail.com>
2026-06-14 00:21:42 +0530

    Time: 12 ms (34.71%), Space: 52.5 MB (85.5%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -4,12 +4,11 @@ class Solution {
         for (int i=0;i<nums.length;i++){
             hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
         }
-        int max=0;
+        int max=Collections.max(hm.values());
         int ans=0;
         for(Integer key:hm.keySet()){
             int curr = hm.get(key);
-            if(curr>max){
-                max=curr;
+            if(curr==max){
                 ans=key;
             }
         }
