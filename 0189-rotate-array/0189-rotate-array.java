5f3a2ab39fce8f65dc65e7012acdfe1b5aed1ff4
Priyanka <priyankaagrawal9139@gmail.com>
2026-06-16 23:43:18 +0530

    Time: 1 ms (41.23%), Space: 63.2 MB (16.66%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -4,7 +4,7 @@ class Solution {
         k=k%nums.length;
         int idx=0;
         for(int i=0;i<nums.length;i++){
-            idx=Math.abs((nums.length-k+i))%nums.length; 
+            idx=(nums.length-k+i)%nums.length; 
             arr[i]=nums[idx];
         }
         for(int i=0;i<nums.length;i++){
