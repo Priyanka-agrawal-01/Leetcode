eba4a0d691e99e7e65d3cfa5cdaf3f6ac1726dfd
Priyanka <priyankaagrawal9139@gmail.com>
2026-06-20 02:43:12 +0530

    Time: 1 ms (99.94%), Space: 77.1 MB (65.11%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -4,8 +4,11 @@ class Solution {
         int max=Integer.MIN_VALUE;
         int curr=0;
         for(int i=0;i<nums.length;i++){
-            curr = Math.max(nums[i], curr + nums[i]);
+            curr+=nums[i];
             max =Math.max(curr,max);
+            if(curr<0){
+                curr=0;
+            }
         }
         return max;
     }
