7b81667104077b9bbf4c5b54797a87d86f35cf09
Priyanka <priyankaagrawal9139@gmail.com>
2026-08-22 18:36:08 +0530

    Time: 7 ms (56.26%), Space: 46.9 MB (78.08%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,37 +1,46 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+
         int indegree[] = new int[numCourses];
+
+        List<List<Integer>> adj = new ArrayList<>();
+
+        for(int i = 0; i < numCourses; i++) {
+            adj.add(new ArrayList<>());
+        }
+
         for(int i = 0; i < prerequisites.length; i++) {
             int course = prerequisites[i][0];
+            int pre = prerequisites[i][1];
 
+            adj.get(pre).add(course);
             indegree[course]++;
         }
+
         Queue<Integer> q = new LinkedList<>();
-        for(int i =0;i<numCourses;i++){
-            if(indegree[i]==0){
+
+        for(int i = 0; i < numCourses; i++) {
+            if(indegree[i] == 0) {
                 q.add(i);
             }
         }
-        int count=0;
+
+        int count = 0;
+
         while(!q.isEmpty()) {
-            int node = q.peek();
-            q.remove();
+            int node = q.remove();
 
             count++;
 
-            for(int i = 0; i < prerequisites.length; i++) {
-                if(prerequisites[i][1] == node) {
-                    int course = prerequisites[i][0];
+            for(int it : adj.get(node)) {
+                indegree[it]--;
 
-                    indegree[course]--;
-
-                    if(indegree[course] == 0) {
-                        q.add(course);
-                    }
+                if(indegree[it] == 0) {
+                    q.add(it);
                 }
             }
         }
-        return count == numCourses;
 
+        return count == numCourses;
     }
 }
