5b984a92ef67375fb277f8ffd0f3d061a85e82e7
Priyanka <priyankaagrawal9139@gmail.com>
2026-09-11 20:50:25 +0530

    Time: 0 ms (100%), Space: 43 MB (19.29%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int ans(int n,int nums[],int[] dp){
+        if(n<0) return 0;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n]=Math.max(nums[n]+ans(n-2,nums,dp),ans(n-1,nums,dp));
+    }
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int dp[]= new int[n];
+        Arrays.fill(dp,-1);
+        return ans(n-1,nums,dp);
+    }
+}
